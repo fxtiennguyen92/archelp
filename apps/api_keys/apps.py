173688565fafb500824f1b1e5dc45fd99064aac3
fxtiennguyen92 <fx.tiennguyen92@gmail.com>
2026-09-03 23:00:46 +0200

    Scaffold Django + GeoDjango, PostGIS via Docker

---
+++ apps/api_keys/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ApiKeysConfig(AppConfig):
+    name = 'apps.api_keys'
---
+++ apps/urbanism_docs/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UrbanismDocsConfig(AppConfig):
+    name = 'apps.urbanism_docs'
